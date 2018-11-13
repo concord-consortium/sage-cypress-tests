0c47f07a485c6b8cd46e89d3c7670c5b6c2594bc
evangelineireland <eireland@concord.org>
2018-11-13 11:47:56 -0800

    Initial commit for model diagram test

--- cypress/integration/model_diagram_test_spec.js
+++ cypress/integration/model_diagram_test_spec.js
@@ -2,5 +2,9 @@ import Document from './elements/Document'
 import SimSettings from './elements/SimSettings'
 
 context('Model Diagram sim setting UI verificaton', function(){
+    describe('Models', function(){
+        it('will add nodes to document', function(){
 
+        })
+    })
 })
